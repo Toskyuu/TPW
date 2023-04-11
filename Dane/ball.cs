f87b47156be33b1a424c934cd86f7946f8779782
Toskyuu <robert.laski10@gmail.com>
2023-04-11 17:11:34 +0200

    Nierówna walka z danymi

---
+++ Dane/ball.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dane
+{
+    public class ball : INotifyPropertyChanged
+    {
+        private int x;
+        private int y;
+        private int radius;
+        private int xSpeed;
+        private int ySpeed;
+
+        public ball(int x, int y, int radius)
+        {
+            this.x = x;
+            this.y = y;
+            this.radius = radius;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged(string name)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(name));
+            }
+        }
+
+        public int Radius
+        {
+            get { return radius; }
+            set
+            {
+                if (radius != value)
+                {
+                    radius = value;
+                    OnPropertyChanged(nameof(Radius));
+                }
+            }
+        }
+
+        public int X
+        {
+            get { return x; }
+            set
+            {
+                if (x != value)
+                {
+                    x = value;
+                    OnPropertyChanged(nameof(x));
+                }
+            }
+        }
+
+        public int Y
+        {
+            get { return y; }
+            set
+            {
+                if (y != value)
+                {
+                    y = value;
+                    OnPropertyChanged(nameof(Y));
+                }
+            }
+        }
+
+
+        public int XSpeed
+        {
+            get { return xSpeed; }
+            set
+            {
+                if (xSpeed != value)
+                {
+                    xSpeed = value;
+                    OnPropertyChanged(nameof(XSpeed));
+                }
+            }
+        }
+
+        public int YSpeed
+        {
+            get { return ySpeed; }
+            set
+            {
+                if (ySpeed != value)
+                {
+                    ySpeed = value;
+                    OnPropertyChanged(nameof(YSpeed));
+                }
+            }
+        }
+
+        public void changePosition(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public void setSpeed(int x, int y)
+        {
+            XSpeed = x;
+            YSpeed = y;
+        }
+
+        
+    }
+}
---
+++ Dane/field.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dane
+{
+    internal class field : INotifyPropertyChanged
+    {
+        private double height;
+        private double width;
+        private readonly List<ball> balls = new List<ball>();
+
+        public field(double height, double width, int numberOfBalls)
+        {
+            this.height = height;
+            this.width = width;
+            CreateBalls(numberOfBalls, 20);
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged(string name)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(name));
+            }
+        }
+
+        public double Height
+        {
+            get { return height; }
+            set
+            {
+                if (height != value)
+                {
+                    height = value;
+                    OnPropertyChanged(nameof(Height));
+                }
+            }
+        }
+
+        public double Width
+        {
+            get { return width; }
+            set
+            {
+                if (width != value)
+                {
+                    width = value;
+                    OnPropertyChanged(nameof(Width));
+                }
+            }
+        }
+
+        public ball CreateBall(int radius)
+        {
+            Random r = new Random();
+            int x = r.Next(20, (int)this.Height - 20);
+            int y = r.Next(20, (int)this.Width - 20);
+
+            int xSpeed = r.Next(-1, 2);
+            int ySpeed = r.Next(-1, 2);
+
+            ball createdBall = new ball(x, y, radius);
+
+            createdBall.setSpeed(xSpeed, ySpeed);
+            return createdBall;
+        }
+        public void CreateBalls(int numberOfBalls, int radius)
+        {
+            this.balls.Clear();
+            for(int i = 0; i < numberOfBalls; i++)
+            {
+                this.balls.Add(CreateBall(radius));
+            }
+        }
+        public List<ball> Balls
+        {
+            get { return balls; }
+        }
+    }
+}
