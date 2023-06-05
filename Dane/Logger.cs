f91df8c4b9a94fc8cad8bf437c6ee3ec0911fcbb
Toskyuu <robert.laski10@gmail.com>
2023-06-05 22:35:48 +0200

    change stopwatch to timer

--- Dane/Logger.cs
+++ Dane/Logger.cs
@@ -1,50 +1,44 @@
 ﻿using System.Diagnostics;
 using System.Text.Json;
+using System.Timers;
+using Timer = System.Timers.Timer;
 
 namespace Dane
 {
     internal class Logger
     {
-        private int index = 0;
-        Stopwatch stopWatch = new Stopwatch();
+        private Timer timer;
+        private List<Ball> balls;
 
 
         public Logger(List<Ball> balls)
         {
-            stopWatch.Start();
-            Task t = new Task(async () =>
-            {
-                while (true)
-                {
-                    if (stopWatch.IsRunning)
-                    {
-                        using (StreamWriter streamWriter =
-                               new StreamWriter(Directory.GetCurrentDirectory() + "\\ballLogs.txt", true))
-                        {
-                            TimeSpan ts = stopWatch.Elapsed;
-                            string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:000}",
-                                ts.Hours, ts.Minutes, ts.Seconds,
-                                ts.Milliseconds);
-                            string msg = ($". Log from {DateTime.UtcNow.ToString("MM/dd/yyyy hh:mm:ss")}\n" +
-                                          $"App running time: " + elapsedTime + "\n");
-                            foreach (Ball ball in balls)
-                            {
-                                streamWriter.WriteLine(index + msg + JsonSerializer.Serialize(ball));
-                                index += 1;
-                            }
-                        }
-                    }
-
-                    await Task.Delay(5);
-                }
-            });
-            t.Start();
+            this.balls = balls;
+            SetTimer();
         }
 
+        private void OnTimedEvent(Object source, ElapsedEventArgs e)
+        {
+            using (StreamWriter streamWriter =
+                   new StreamWriter(Directory.GetCurrentDirectory() + "\\ballLogs.txt", true))
+            {
+                string msg = ("Log from " + e.SignalTime + "\n");
+                foreach (Ball ball in balls)
+                {
+                    streamWriter.WriteLine(msg + JsonSerializer.Serialize(ball)); }
+            }
+        }
+
+        private void SetTimer()
+        {
+            timer = new Timer(100);
+            timer.Elapsed += OnTimedEvent;
+            timer.AutoReset = true;
+            timer.Enabled = true;
+        }
         public void Stop()
         {
-            stopWatch.Stop();
-            index = 0;
+            timer.Stop();
         }
     }
 }
